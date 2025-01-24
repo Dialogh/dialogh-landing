26bea7e4f1004dfad96a7a91cceaa3df68ba7fc2
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-01-25 02:29:40 +0530

    Refactor components and update dependencies: replace local images with updated assets, enhance footer links, and optimize layout scripts for better performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
-import tailwind from '@astrojs/tailwind';
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
-import react from '@astrojs/react';
-
-import svelte from '@astrojs/svelte';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), svelte()]
+  integrations: [
+    tailwind(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    playformCompress(),
+  ],
 });
